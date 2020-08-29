405bc0d4617afcb3f3494bc2db8b1460d7de5bbe
K.M Ahnaf Zamil <ahnafzamil896@gmail.com>
2020-08-29 15:36:41 +0600

    Part 3

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, session, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
@@ -21,7 +21,15 @@ class Posts(db.Model):
     slug = db.Column(db.String(255))
 
 
-admin.add_view(ModelView(Posts, db.session))
+class SecureModelView(ModelView):
+    def is_accessible(self):
+        if "logged_in" in session:
+            return True
+        else:
+            abort(403)
+
+
+admin.add_view(SecureModelView(Posts, db.session))
 
 
 @app.route("/")
@@ -47,6 +55,23 @@ def post(slug):
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        if request.form.get("username") == "Ahnaf" and request.form.get("password") == "iamahnaf@12345":
+            session['logged_in'] = True
+            return redirect("/admin")
+        else:
+            return render_template("login.html", failed=True)
+    return render_template("login.html")  
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
 
 
 if __name__ == "__main__":
