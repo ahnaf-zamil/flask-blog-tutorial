53af41f7ce645c257dbf0d674d95e63f103fa6d4
K.M Ahnaf Zamil <ahnafzamil896@gmail.com>
2020-08-30 17:44:24 +0600

    Part 4

--- app.py
+++ app.py
@@ -3,10 +3,21 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 import sqlalchemy
+from flask_mail import Mail, Message
+from config import mail_username, mail_password
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root@localhost/flask_blog"
 app.config['SECRET_KEY'] = "mysecretkeywhichissupposedtobesecret"
+app.config['MAIL_SERVER'] = "smtp-mail.outlook.com"
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = mail_username
+app.config['MAIL_PASSWORD'] = mail_password
+
+mail = Mail(app)
 db = SQLAlchemy(app)
 admin = Admin(app)
 
@@ -52,20 +63,31 @@ def post(slug):
         abort(404)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
+    if request.method == "POST":
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+
+        msg = Message(
+            subject=f"Mail from {name}", body=f"Name: {name}\nE-Mail: {email}\nPhone: {phone}\n\n\n{message}", sender=mail_username, recipients=['yourpersonalemail@protonmail.com'])
+        mail.send(msg)
+        return render_template("contact.html", success=True)
+
     return render_template("contact.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        if request.form.get("username") == "Ahnaf" and request.form.get("password") == "iamahnaf@12345":
+        if request.form.get("username") == "your_admin_username" and request.form.get("password") == "your_admin_password":
             session['logged_in'] = True
             return redirect("/admin")
         else:
             return render_template("login.html", failed=True)
-    return render_template("login.html")  
+    return render_template("login.html")
 
 
 @app.route("/logout")
